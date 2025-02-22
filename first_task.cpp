07906ede0385c5f1b2323b15d823339cd324ed46
VladDuplava <duplava.vladyslav@chnu.edu.ua>
2025-02-22 17:01:15 +0200

    Update first_task.cpp

--- first_task.cpp
+++ first_task.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
-#include <vector>
-#include <limits>
 
 int main() {
     int N;
     std::cout << "Введіть розмір масиву: ";
     std::cin >> N;
 
-    std::vector<int> A(N);
+    int A[N];
     std::cout << "Введіть елементи масиву: ";
-    for (int &x : A) std::cin >> x;
-
-    int min_positive = std::numeric_limits<int>::max(), min_index = -1;
-
-    
     for (int i = 0; i < N; i++) {
-        if (A[i] > 0 && A[i] < min_positive) {
+        std::cin >> A[i];
+    }
+
+    int min_positive = -1, min_index = -1;
+
+    for (int i = 0; i < N; i++) {
+        if (A[i] > 0 && (min_positive == -1 || A[i] < min_positive)) {
             min_positive = A[i];
             min_index = i;
         }
     }
-
 
     if (min_index != -1) {
         std::cout << "Мінімальний додатний елемент: " << min_positive << ", Індекс: " << min_index << std::endl;
